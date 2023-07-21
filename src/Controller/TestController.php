a8176e5b2d4950046fde1ca605fce0937096d4c9
roman <roman.lotocki@gmail.com>
2023-07-21 12:07:23 -0400

    tests setup OK

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
+        xdebug_info();
         // exemple de comment récupérer la liste des jeux video depuis le user courant. Il faut itérer sur la collection d'objets Doctrine. La solution ne semble pas judicieuse. Elle implique une itération dans le code et met le meme temps (voir plus) qu'une fonction du repo qui fetcherait directement depuis la data base. Peut etre que si le nombres d'items en BDD devient très important cette solution sera mieux adaptée.
         // $user = $this->getUser();
         
---
+++ tests/Entity/VideoGamePhpTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\VideoGame;
+use DateTimeImmutable;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class VideoGameTestPhpTest extends KernelTestCase
+{
+    public function testSomething(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $vg = new VideoGame();
+        $vg->setName('Wild Arms')
+            ->setConsole('PlayStation')
+            ->setReleaseDate(new DateTimeImmutable('10/01/1998'))
+            ->setCountry('Europe')
+            ->setCreatedAt(new DateTimeImmutable());
+
+        $errors = $container->get('validator')->validate($vg);
+        $this->assertCount(0, $errors);
+
+    }
+
+
+}
